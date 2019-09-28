d7017e30fbce63135e9ff8cd73cadb975d8e98a8
Hannah Zuber <hgomberg@gmail.com>
2019-09-28 23:57:16 +0300

    refactored functions

--- asanaTasks.js
+++ asanaTasks.js
@@ -2,10 +2,15 @@ chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     const taskArr = document.getElementsByClassName("autogrowTextarea-input");
     const taskName = taskArr.length === 0 ? alert("Please click on the task you wish to add to or delete from your list.") : taskArr[0].innerHTML;
-    const projectArr = document.getElementsByClassName("ProjectPageHeader-projectName");
-    const projectName = projectArr.length === 0 ? "" : projectArr[0].innerText;
+    const projectArr = document.getElementsByClassName("TaskProjectPill-projectName");
+    console.log(projectArr);
+    const projectName = projectArr.length === 0 ? "N/A" : 
+                        projectArr.length === 1 ? projectArr[0].firstChild.innerHTML : 
+                        projectArr[0].innerHTML === projectArr[1].innerHTML ? projectArr[0].firstChild.innerHTML : 
+                        (`${projectArr[0].firstChild.innerHTML} / ${projectArr[1].firstChild.innerHTML}` );
+                        console.log(projectName);
     const columnArr = document.querySelectorAll(".TaskProjectSectionInfo-sectionDropdown, .TaskProjectColumnInfo-dropdown");
-    const columnName = columnArr.length === 0 ? "" : 
+    const columnName = columnArr.length === 0 ? "N/A" : 
                         columnArr.length === 1 ? columnArr[0].firstChild.innerHTML : 
                         columnArr[0].innerHTML === columnArr[1].innerHTML ? columnArr[0].firstChild.innerHTML : 
                         (`${columnArr[0].firstChild.innerHTML} / ${columnArr[1].firstChild.innerHTML}` );
